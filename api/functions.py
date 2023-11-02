7659b4f3c90a2b1560408b8738e351754cfb3b25
Zach Calvert <zacharym.calvert@gmail.com>
2023-11-02 16:33:54 -0700

    Add pythagorean triplet function

--- api/functions.py
+++ api/functions.py
@@ -6,3 +6,13 @@ def sum_of_squares(number):
 def square_of_sums(number):
     """Given a number n, return the square of the sum of the first n natural numbers"""
     return sum([i for i in range(number + 1)]) ** 2
+
+
+def is_pythagorean_triplet(a, b, c):
+    """Given a sequence of three numbers, return whether they together make a Pythagorean triplet"""
+    return a**2 + b**2 == c**2
+
+
+def product_of_three(a, b, c):
+    """Given three numbers, return their product"""
+    return a*b*c
