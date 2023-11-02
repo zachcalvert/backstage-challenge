3a4da6c3615bc22f0601c250ec2688a51db5ba8e
Zach Calvert <zacharym.calvert@gmail.com>
2023-11-02 16:34:13 -0700

    Add pythagorean triplet endpoint

--- api/tests.py
+++ api/tests.py
@@ -4,8 +4,8 @@ from django.test import TestCase
 from django.test.client import Client
 from django.urls import reverse
 
-from api.functions import sum_of_squares, square_of_sums
-from api.models import DifferenceRequest
+from api.functions import sum_of_squares, square_of_sums, is_pythagorean_triplet
+from api.models import DifferenceRequest, PythagoreanTripletRequest
 
 
 class SumOfSquaresTestCase(TestCase):
@@ -44,6 +44,52 @@ class DifferenceTestCase(TestCase):
 
     def test_bad_value(self):
         url = self.url + "?number=x"
+
+        response = self.client.get(url)
+        assert response.status_code == 400
+        assert DifferenceRequest.objects.count() == 0
+
+
+class PythagoreanTripletTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse("pythagorean_triplet")
+
+    def test_is_pythagorean_triplet(self):
+        assert PythagoreanTripletRequest.objects.count() == 0
+
+        url = self.url + "?a=3&b=4&c=5"
+        response = self.client.get(url)
+        assert response.status_code == 200
+
+        content = json.loads(response.content)
+        assert set(content.keys()) == {'datetime', 'is_triplet', 'last_datetime', 'product', 'occurrences'}
+        assert content["is_triplet"] is True
+        assert content["product"] == 60
+        assert PythagoreanTripletRequest.objects.count() == 1
+
+    def test_is_not_pythagorean_triplet(self):
+        assert PythagoreanTripletRequest.objects.count() == 0
+
+        url = self.url + "?a=1&b=2&c=3"
+        response = self.client.get(url)
+        assert response.status_code == 200
+
+        content = json.loads(response.content)
+        assert set(content.keys()) == {'datetime', 'is_triplet', 'last_datetime', 'product', 'occurrences'}
+        assert content["is_triplet"] is False
+        assert content["product"] == 6
+        assert PythagoreanTripletRequest.objects.count() == 1
+
+    def test_bad_value(self):
+        url = self.url + "?a=3&b=4&c=five"
+
+        response = self.client.get(url)
+        assert response.status_code == 400
+        assert DifferenceRequest.objects.count() == 0
+
+    def test_missing_params(self):
+        url = self.url + "?a=3&b=4"
 
         response = self.client.get(url)
         assert response.status_code == 400
--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from api import views
 
 urlpatterns = [
     path("difference", views.difference, name="difference"),
+    path("pythagorean_triplet", views.pythagorean_triplet, name="pythagorean_triplet")
 ]
